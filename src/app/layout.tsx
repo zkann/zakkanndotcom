87cb89333062e20eea30347b10ffbc616ed18002
thekannman <zrkann@gmail.com>
2025-08-15 15:11:10 -0500

    meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -43,36 +42,30 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* Meta Pixel base code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-          !(function(f,b,e,v,n,t,s){
-            if(f.fbq)return; n=f.fbq=function(){ n.callMethod ?
-            n.callMethod.apply(n,arguments) : n.queue.push(arguments) };
-            if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0';
-            n.queue=[]; t=b.createElement(e); t.async=!0;
-            t.src=v; s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)
-          })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${META_PIXEL_ID}');
-          fbq('track', 'PageView');
-        `}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=635050849636654&ev=PageView&noscript=1"
+        /></noscript>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        {/* Meta Pixel <noscript> fallback must be <img>, so we disable rules here. */}
-        <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
         <Analytics />
       </body>
     </html>
