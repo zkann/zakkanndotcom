032a4ac3a9615eac3481493ec4e2cfeaba3b9753
thekannman <zrkann@gmail.com>
2025-08-15 19:36:55 -0500

    revert pixel change

--- src/app/(site)/book/page.tsx
+++ src/app/(site)/book/page.tsx
@@ -1,16 +1,6 @@
-import { shouldEnableTracking } from '@/lib/env';
-import BookPageClient from './BookPageClient';
+import { redirect } from 'next/navigation';
 
 export default function BookPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
-        <p className="text-gray-600">Redirecting to booking page...</p>
-        
-        {/* Only render client component if tracking is enabled */}
-        {shouldEnableTracking && <BookPageClient />}
-      </div>
-    </div>
-  );
+  // This will redirect on the server side, preventing the RSC fetch error
+  redirect('https://calendly.com/zakkann/30min');
 } 
